7bbe0a125040521a31896a95a780b018ddae5973
Sven Schmidt <svenschmidt75@googlemail.com>
2016-04-02 10:16:46 -0600

    minor changes

--- Haskell/TheCraftOfFunctionalProgramming_SimonThompson/horse/Pictures.hs
+++ Haskell/TheCraftOfFunctionalProgramming_SimonThompson/horse/Pictures.hs
@@ -1,10 +1,29 @@
 module Pictures where
 
+-- page 21
+
 type Picture = [String]
 
 horse :: Picture
-horse = [".......##...", ".....##..#.."]
+horse = [".......##...",
+         ".....##..#..",
+         "...##.....#.",
+         "..#.......#.",
+         "..#...#...#.",
+         "..#...###.#.",
+         ".#....#..##.",
+         "..#...#.....",
+         "...#...#....",
+         "....#..#....",
+         ".....#.#....",
+         "......##...."]
 
 -- mapM_ :: Monad m => (a -> m b) -> [a] -> m ()
 printPicture :: Picture -> IO ()
 printPicture = mapM_ putStrLn
+
+flipH :: Picture -> Picture
+flipH = reverse
+
+flipV :: Picture -> Picture
+flipV picture = [ reverse line | line <- picture ]
