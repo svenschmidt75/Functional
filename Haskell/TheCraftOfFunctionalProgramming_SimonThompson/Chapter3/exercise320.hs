1eac1319e39da6604c043b69f0ab5ace7606a410
Sven Schmidt <svenschmidt75@googlemail.com>
2016-04-09 11:54:30 -0600

    minor changes

--- Haskell/TheCraftOfFunctionalProgramming_SimonThompson/Chapter3/exercise320.hs
+++ Haskell/TheCraftOfFunctionalProgramming_SimonThompson/Chapter3/exercise320.hs
@@ -13,11 +13,24 @@ import Test.QuickCheck
 averageThree :: Integer -> Integer -> Integer -> Float
 averageThree a b c = fromIntegral (a + b + c) / 3.0
 
---prop_exercise316 :: Char -> Bool
---prop_exercise316 ch = toUpperCase ch == toUpper ch
+howManyAboveAverage :: Integer -> Integer -> Integer -> Integer
+howManyAboveAverage a b c = let average = averageThree a b c in
+                            let lst = map fromInteger [a, b, c] in 
+                            toInteger $ length $ filter (>average) lst
+
+prop_exercise320 :: Integer -> Integer -> Integer -> Bool
+prop_exercise320 a b c = averageThree a b c == fromInteger (sum [a, b, c]) / 3
+
+prop_exercise321 :: Integer -> Integer -> Integer -> Bool
+prop_exercise321 a b c = howManyAboveAverage a b c == 
+                                                      let lst = map fromInteger [a, b, c] in
+                                                      (toInteger $ length $ filter (>averageThree a b c) lst)
 
 --main = quickCheck prop_exercise316
 
 main :: IO ()
 main = do
-        putStrLn $ show $ averageThree 1 2 9
+        print (averageThree 1 6 9)
+        putStrLn $ show $ howManyAboveAverage 1 6 9
+        quickCheck prop_exercise320
+        quickCheck prop_exercise321
