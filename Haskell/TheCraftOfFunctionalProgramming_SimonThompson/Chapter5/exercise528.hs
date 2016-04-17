2b4c2c9a8b288ac3c18f32664a7d25dbe34042fc
Sven Schmidt <svenschmidt75@googlemail.com>
2016-04-17 09:40:25 -0600

    more exercises from chapter 5

--- Haskell/TheCraftOfFunctionalProgramming_SimonThompson/Chapter5/exercise528.hs
+++ Haskell/TheCraftOfFunctionalProgramming_SimonThompson/Chapter5/exercise528.hs
@@ -18,12 +18,21 @@ borrowed db book = case borrowers db book of
                     []    -> False
                     (_:_) -> True
 
-
-{-
 numBorrowed :: Database -> Person -> Int
-makeLoad :: Database -> Person -> Book -> Database
-returnLoad :: Database -> Person -> Book -> Database
--}
+numBorrowed db person = length $ books db person
+
+makeLoan :: Database -> Person -> Book -> Database
+makeLoan db person book = (person, book) : db
+
+removeFromList :: Eq a => a -> [a] -> [a]
+removeFromList x xs = case xs of
+    [] -> []
+    (y:ys) -> if x == y then removeFromList x ys else y : removeFromList x ys
+
+returnLoan :: Database -> Person -> Book -> Database
+returnLoan db person book = case elem person (borrowers db book) of
+                                True -> removeFromList (person, book) db
+                                False -> db
 
 
 
@@ -37,3 +46,11 @@ main = do
     print tmp2
     let tmp3 = borrowed [("Alice", "Book1"), ("Alice", "Book2")] "Book4"
     print tmp3
+    let tmp4 = numBorrowed [("Alice", "Book1"), ("Alice", "Book2")] "Alice"
+    print tmp4
+    let tmp5 = borrowers (makeLoan [("Alice", "Book1"), ("Alice", "Book2")] "Alice" "Book3") "Book4"
+    print tmp5
+    let tmp6 = makeLoan [("Alice", "Book1"), ("Alice", "Book2")] "Alice" "Book3"
+    print tmp6
+    let tmp7 = returnLoan tmp6 "Alice" "Book3"
+    print tmp7
