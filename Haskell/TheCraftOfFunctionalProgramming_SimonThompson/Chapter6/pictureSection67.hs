43fd541b28d93f2bf1f70ff5eee726f53c93e6a8
Sven Schmidt <svenschmidt75@googlemail.com>
2016-05-01 07:30:33 -0600

    done all formatting

--- Haskell/TheCraftOfFunctionalProgramming_SimonThompson/Chapter6/pictureSection67.hs
+++ Haskell/TheCraftOfFunctionalProgramming_SimonThompson/Chapter6/pictureSection67.hs
@@ -19,12 +19,9 @@ codeIndex = [(4719, "Fish Fingers",       121),
 type TillType = [BarCode]
 type BillType = [(Name, Price)]
 
---makeBill :: TillType -> BillType
-
-
 formatPence :: Price -> String
-formatPence pence = let integer_part = pence `div` 100 in
-                    let fractional_part = pence `mod` 100 in
+formatPence price = let integer_part = price `div` 100 in
+                    let fractional_part = price `mod` 100 in
                     let formatted_integer_part = show integer_part in
                     let formatted_fractional_part = show fractional_part in
                     formatted_integer_part ++ "." ++ (padWithZero formatted_fractional_part)
@@ -33,7 +30,30 @@ formatPence pence = let integer_part = pence `div` 100 in
                         | length formatted_fractional_part == 2 = formatted_fractional_part
                         | otherwise                             = (replicate (2 - length formatted_fractional_part) '0') ++ formatted_fractional_part
 
---formatBill :: BillType -> String
+formatLine :: (Name, Price) -> String
+formatLine (name, price) = let ln = length name in
+                           let lp = length (formatPence price) in
+                           let diff = lineLength - (ln + lp) in
+                           name ++ replicate diff '.' ++ (formatPence price) ++ "\n"
+
+formatLines :: [(Name, Price)] -> String
+--formatLines (x:xs) = formatLine x ++ formatLines xs
+--formatLines []     = ""
+formatLines items = foldr (\item accum -> accum ++ formatLine item) "" items
+
+makeTotal :: BillType -> Price
+makeTotal ((_, price):xs) = price + makeTotal xs
+makeTotal []              = 0
+
+formatTotal :: Price -> String
+formatTotal price = "\n" ++ formatLine ("Total", price)
+
+formatBill :: BillType -> String
+formatBill bill = formatLines bill ++ formatTotal (makeTotal bill)
+
+
+
+--makeBill :: TillType -> BillType
 
 
 --produceBill :: TillType -> String
