02d5668e9e65a1c0889aeff4efe3d66ea20a25d1
Sven Schmidt <svenschmidt75@googlemail.com>
2016-06-18 10:02:25 -0600

    more exercises

--- Haskell/TheCraftOfFunctionalProgramming_SimonThompson/Chapter7/Exercise7.hs
+++ Haskell/TheCraftOfFunctionalProgramming_SimonThompson/Chapter7/Exercise7.hs
@@ -1,7 +1,7 @@
 module Main where
 
-import Prelude hiding (product, and, or)
-import qualified Prelude (product, and, or)
+import Prelude hiding (product, and, or, reverse, unzip)
+import qualified Prelude (product, and, or, reverse, unzip)
 
 import Test.QuickCheck
 
@@ -64,6 +64,33 @@ elemNum item (x:xs)
     | item == x = 1 + elemNum item xs
     | otherwise = elemNum item xs
 
+elemNum_primitives :: Integer -> [Integer] -> Integer
+elemNum_primitives item xs = toInteger $ length $ filter (== item) xs
+
+elemNum_listComprehension :: Integer -> [Integer] -> Integer
+elemNum_listComprehension item xs = toInteger $ length $ [x | x <- xs, x == item]
+
+-- exercise 7.9
+unique :: [Integer] -> [Integer]
+unique [] = []
+unique xs = filter (\x -> elemNum x xs == 1) xs
+
+unique_listComprehension :: [Integer] -> [Integer]
+unique_listComprehension xs = [x | x <- xs, elemNum x xs == 1]
+
+-- exercise 7.11
+reverse :: [a] -> [a]
+reverse [] = []
+reverse (x:xs) = reverse xs ++ [x]
+
+prop_reverse :: Eq a => [a] -> Bool
+prop_reverse xs = reverse xs == Prelude.reverse xs
+
+unzip :: [(a, b)] -> ([a], [b])
+unzip [] = []
+unzip ((x, y):xs) = 
+
+
 main = do
     print $ excercise71 [0, 1]
     print $ excercise72 [1, 2]
@@ -74,3 +101,9 @@ main = do
     quickCheck prop_exercise76
     quickCheck prop_exercise77
     print $ elemNum 7 [1, 7, 5, 4, 3, 7, 8, 9, 7, 9]
+    print $ elemNum_primitives 7 [1, 7, 5, 4, 3, 7, 8, 9, 7, 9]
+    print $ elemNum_listComprehension 7 [1, 7, 5, 4, 3, 7, 8, 9, 7, 9]
+    print $ unique [4, 2, 1, 3, 2, 3]
+    print $ unique_listComprehension [4, 2, 1, 3, 2, 3]
+    print $ reverse [1, 2, 3, 4]
+    --quickCheck prop_reverse
