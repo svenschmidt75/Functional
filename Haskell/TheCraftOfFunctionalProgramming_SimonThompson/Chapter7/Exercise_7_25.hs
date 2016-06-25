ce2c2c5d94d12bb13291389bc44c32ce32555364
Sven Schmidt <svenschmidt75@googlemail.com>
2016-06-25 08:56:45 -0600

    exercise 7.25

--- Haskell/TheCraftOfFunctionalProgramming_SimonThompson/Chapter7/Exercise_7_25.hs
+++ Haskell/TheCraftOfFunctionalProgramming_SimonThompson/Chapter7/Exercise_7_25.hs
@@ -9,7 +9,7 @@ import Test.QuickCheck
 -- Exercise 7.25
 is_sublist :: String -> String -> Bool
 is_sublist [] _  = False
-is_sublist es [] = False
+is_sublist _  [] = False
 is_sublist (e:es) (s:ss)
     | e == s    = is_sublist' es ss
     | otherwise = is_sublist (e:es) ss
@@ -20,7 +20,22 @@ is_sublist (e:es) (s:ss)
                 | e == s    = is_sublist' es ss
                 | otherwise = is_sublist' (e:es) ss
 
+is_subsequence :: String -> String -> Bool
+is_subsequence [] _  = False
+is_subsequence _  [] = False
+is_subsequence (e:es) (s:ss)
+    | e == s    = is_subsequence' es ss
+    | otherwise = is_subsequence (e:es) ss
+        where
+            is_subsequence' [] _  = True
+            is_subsequence' es [] = False
+            is_subsequence' (e:es) (s:ss) 
+                | e == s    = is_subsequence' es ss
+                | otherwise = False
+
 main :: IO ()
 main = do
     print $ is_sublist "ship" "Fish & Chips"
+    print $ is_subsequence  "Chip" "Fish & Chips"
+    print $ is_subsequence  "Chip" "Fish & Chips"
     print "Done"
