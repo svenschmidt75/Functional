1a88979b06f0ebde79beddcbe73d03944776aac9
Sven Schmidt <s.schmidt@questintegrity.com>
2016-07-31 10:51:22 -0400

    implement map for tree

--- Haskell/HaskellProgramming/Chapter11/BinaryTree/src/Lib.hs
+++ Haskell/HaskellProgramming/Chapter11/BinaryTree/src/Lib.hs
@@ -2,6 +2,7 @@ module Lib
     ( someFunc,
       BinaryTree (..),
       insert',
+      mapTree,
     ) where
 
 data BinaryTree a = 
@@ -12,8 +13,13 @@ data BinaryTree a =
 insert' :: Ord a => a -> BinaryTree a -> BinaryTree a
 insert' a Leaf  = Node Leaf a Leaf
 insert' a (Node bl value br)
-    | a <= value = Node (insert' a bl) value br
+    | a == value = Node bl a br
+    | a < value  = Node (insert' a bl) value br
     | otherwise  = Node bl             value (insert' a br)
+
+mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+mapTree _ Leaf               = Leaf
+mapTree f (Node bl value br) = Node (mapTree f bl) (f value) (mapTree f br)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- Haskell/HaskellProgramming/Chapter11/BinaryTree/test/InsertSpec.hs
+++ Haskell/HaskellProgramming/Chapter11/BinaryTree/test/InsertSpec.hs
@@ -4,6 +4,7 @@ module InsertSpec (spec) where
 import Lib
        ( BinaryTree (..)
        , insert'
+       , mapTree
        )
 
 import Test.Hspec
@@ -12,7 +13,7 @@ import Test.Hspec.QuickCheck
 
 spec :: Spec
 spec = do
-    describe "BinaryTree" $ do
+    describe "BinaryTree Insert" $ do
         it "create from empty tree" $ do
             (insert' 1 Leaf) `shouldBe` (Node (Leaf) 1 (Leaf))
 
@@ -23,10 +24,24 @@ spec = do
 
         it "add same" $ do
             let bt = Node Leaf 10 Leaf
-            let expected = Node (Node Leaf 1 Leaf) 10 Leaf
-            (insert' 1 bt) `shouldBe` expected
+            let expected = Node Leaf 10 Leaf
+            (insert' 10 bt) `shouldBe` expected
 
         it "add bigger" $ do
             let bt = Node Leaf 10 Leaf
             let expected = Node Leaf 10 (Node Leaf 11 Leaf)
             (insert' 11 bt) `shouldBe` expected
+
+    describe "BinaryTree Map" $ do
+        it "map on empty tree" $ do
+            (mapTree (+1) Leaf) `shouldBe` Leaf
+
+        it "map on non-empty tree" $ do
+            let bt = Node Leaf 10 (Node Leaf 6 Leaf)
+            let expected = Node Leaf 11 (Node Leaf 7 Leaf)
+            (mapTree (+1) bt) `shouldBe` expected
+
+        it "map on non-empty tree 2" $ do
+            let bt = Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf)
+            let expected = Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
+            (mapTree (+1) bt) `shouldBe` expected
