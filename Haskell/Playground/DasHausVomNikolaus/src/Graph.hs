6a38716bbf4e4565929631490af6a49ae7749c97
Sven Schmidt <s.schmidt@questintegrity.com>
2016-08-13 10:39:04 -0600

    add vertex and edge support for undirected graph

--- Haskell/Playground/DasHausVomNikolaus/src/Graph.hs
+++ Haskell/Playground/DasHausVomNikolaus/src/Graph.hs
@@ -3,15 +3,22 @@ module Graph
     , Edge (..)
     , Graph (..)
     , hasVertex
+    , addVertex
+    , hasEdge
+    , addEdge
     ) where
 
 -- Each vertex has a unique index
 data Vertex = Vertex Int
+        deriving (Eq, Show)
 
 -- An edge connects two vertices
 data Edge = Edge Vertex Vertex
+        deriving (Eq, Show)
 
+-- should this type be UndirectedGraph?
 data Graph = Graph [Vertex] [Edge]
+        deriving (Eq, Show)
 
 hasVertex :: Vertex -> Graph -> Bool
 hasVertex (Vertex i) (Graph vertices _) = hasVertex' vertices
@@ -21,6 +28,24 @@ hasVertex (Vertex i) (Graph vertices _) = hasVertex' vertices
                                               | x == i    = True
                                               | otherwise = hasVertex' xs
 
+addVertex :: Vertex -> Graph -> Graph
+addVertex v g@(Graph vs es)
+    | hasVertex v g = g
+    | otherwise     = Graph (v:vs) es
 
---addVertex :: Vertex -> Graph -> Graph
---addVertex v g
+-- undirected edges
+hasEdge :: Edge -> Graph -> Bool
+hasEdge (Edge v1 v2) (Graph _ edges) = hasEdge' edges
+                                          where
+                                            hasEdge' [] = False
+                                            hasEdge' (Edge x1 x2:es)
+                                              | x1 == v1 && x2 == v2 = True
+                                              | x1 == v2 && x2 == v1 = True
+                                              | otherwise            = hasEdge' es
+
+addEdge :: Edge -> Graph -> Graph
+addEdge e@(Edge v1 v2) g@(Graph vs es)
+    | hasEdge e g             = g
+    | hasVertex v1 g == False = error $ "addEdge: Vertex " ++ show v1 ++ " noy in graph"
+    | hasVertex v2 g == False = error $ "addEdge: Vertex " ++ show v2 ++ " noy in graph"
+    | otherwise               = Graph vs (e:es)
--- Haskell/Playground/DasHausVomNikolaus/test/GraphSpec.hs
+++ Haskell/Playground/DasHausVomNikolaus/test/GraphSpec.hs
@@ -6,6 +6,9 @@ import Graph
        , Edge (..)
        , Graph (..)
        , hasVertex
+       , addVertex
+       , hasEdge
+       , addEdge
        )
 
 import Test.Hspec
@@ -15,6 +18,50 @@ import Test.Hspec.QuickCheck
 spec :: Spec
 spec = do
     describe "Vertex" $ do
-        it "hasVertex" $ do
+        it "hasVertex - does not yet exist" $ do
             let graph = Graph [Vertex 1] [Edge (Vertex 1) (Vertex 2)]
             hasVertex (Vertex 2) graph `shouldBe` False
+
+        it "hasVertex - exists" $ do
+            let graph = Graph [Vertex 1] [Edge (Vertex 1) (Vertex 2)]
+            hasVertex (Vertex 1) graph `shouldBe` True
+
+        it "addVertex - does not yet exist" $ do
+            let isGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            let expectedGraph = Graph [Vertex 3, Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            addVertex (Vertex 3) isGraph `shouldBe` expectedGraph
+
+        it "addVertex - exists" $ do
+            let isGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            let expectedGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            addVertex (Vertex 1) isGraph `shouldBe` expectedGraph
+            addVertex (Vertex 2) isGraph `shouldBe` expectedGraph
+
+
+    describe "Edge" $ do
+        it "hasEdge - does not yet exist" $ do
+            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            hasEdge (Edge (Vertex 3) (Vertex 2)) graph `shouldBe` False
+
+        it "hasEdge - exist" $ do
+            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            hasEdge (Edge (Vertex 1) (Vertex 2)) graph `shouldBe` True
+
+        it "hasEdge - exist, undirected" $ do
+            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            hasEdge (Edge (Vertex 2) (Vertex 1)) graph `shouldBe` True
+
+        it "addEdge - does not yet exist" $ do
+            let isGraph = Graph [Vertex 1, Vertex 2] []
+            let expectedGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            addEdge (Edge (Vertex 1) (Vertex 2)) isGraph `shouldBe` expectedGraph
+
+        it "addEdge - exists" $ do
+            let isGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            let expectedGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            addEdge (Edge (Vertex 1) (Vertex 2)) isGraph `shouldBe` expectedGraph
+
+        it "addEdge - exists, undirected" $ do
+            let isGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            let expectedGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            addEdge (Edge (Vertex 2) (Vertex 1)) isGraph `shouldBe` expectedGraph
