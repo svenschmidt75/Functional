d7f61604321c51fd96f93fc4d2a2656dc4c8587c
Sven Schmidt <s.schmidt@questintegrity.com>
2016-08-20 11:32:07 -0600

    minor changes

--- Haskell/Playground/DasHausVomNikolaus/src/Lib.hs
+++ Haskell/Playground/DasHausVomNikolaus/src/Lib.hs
@@ -4,10 +4,23 @@ module Lib
 
 import Graph
        ( Vertex (..)
+       , Edge (..)
        , Graph (..)
+       , neighbors
+       , isEdgeColored
        )
 
+import Data.Maybe (isNothing)
 
+
+getUncoloredEdges :: Vertex -> Graph -> [Edge]
+getUncoloredEdges v g =
+                    let neighboringVertices = neighbors v g
+                        edges               = [Edge v y 0 | y <- neighboringVertices]
+                        nonColoredEdges     = filter (\(Edge v1 v2 _) -> isNothing $ isEdgeColored v1 v2 g) edges
+                    in nonColoredEdges
 
 solver :: Graph -> [[Vertex]]
-solver = undefined
+solver g = do
+       let (Graph vs es) = g
+       return []
