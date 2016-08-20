0c841c1f42f398bc1ea2d514a81d7044d17d7409
Sven Schmidt <s.schmidt@questintegrity.com>
2016-08-20 08:43:27 -0600

    add color to edges

--- Haskell/Playground/DasHausVomNikolaus/src/Graph.hs
+++ Haskell/Playground/DasHausVomNikolaus/src/Graph.hs
@@ -14,8 +14,10 @@ module Graph
 data Vertex = Vertex Int
         deriving (Eq, Show)
 
+type Color = Int
+
 -- An edge connects two vertices
-data Edge = Edge Vertex Vertex
+data Edge = Edge Vertex Vertex Color
         deriving (Eq, Show)
 
 -- should this type be UndirectedGraph?
@@ -37,16 +39,16 @@ addVertex v g@(Graph vs es)
 
 -- undirected edges
 hasEdge :: Edge -> Graph -> Bool
-hasEdge (Edge v1 v2) (Graph _ edges) = hasEdge' edges
-                                          where
-                                            hasEdge' [] = False
-                                            hasEdge' (Edge x1 x2:es)
-                                              | x1 == v1 && x2 == v2 = True
-                                              | x1 == v2 && x2 == v1 = True
-                                              | otherwise            = hasEdge' es
+hasEdge (Edge v1 v2 _) (Graph _ edges) = hasEdge' edges
+                                           where
+                                             hasEdge' [] = False
+                                             hasEdge' (Edge x1 x2 _ : es)
+                                               | x1 == v1 && x2 == v2 = True
+                                               | x1 == v2 && x2 == v1 = True
+                                               | otherwise            = hasEdge' es
 
 addEdge :: Edge -> Graph -> Graph
-addEdge e@(Edge v1 v2) g@(Graph vs es)
+addEdge e@(Edge v1 v2 _) g@(Graph vs es)
     | hasEdge e g             = g
     | hasVertex v1 g == False = error $ "addEdge: Vertex " ++ show v1 ++ " not in graph"
     | hasVertex v2 g == False = error $ "addEdge: Vertex " ++ show v2 ++ " not in graph"
@@ -54,14 +56,14 @@ addEdge e@(Edge v1 v2) g@(Graph vs es)
 
 --adjacent(G, x, y): tests whether there is an edge from the vertices x to y;
 adjacent :: Vertex -> Vertex -> Graph -> Bool
-adjacent v1 v2 = hasEdge (Edge v1 v2)
+adjacent v1 v2 = hasEdge (Edge v1 v2 0)
 
 --neighbors(G, x): lists all vertices y such that there is an edge from the vertices x to y;
 neighbors :: Vertex -> Graph -> [Vertex]
 neighbors vertex (Graph _ edges) = neighbors' edges []
                                  where
                                    neighbors' [] vs = vs
-                                   neighbors' (Edge v1 v2 : es) vs
+                                   neighbors' (Edge v1 v2 _ : es) vs
                                        | v1 == vertex = v2 : neighbors' es vs
                                        | v2 == vertex = v1 : neighbors' es vs
                                        | otherwise    =      neighbors' es vs
--- Haskell/Playground/DasHausVomNikolaus/test/GraphSpec.hs
+++ Haskell/Playground/DasHausVomNikolaus/test/GraphSpec.hs
@@ -21,83 +21,83 @@ spec :: Spec
 spec = do
     describe "Vertex" $ do
         it "hasVertex - does not yet exist" $ do
-            let graph = Graph [Vertex 1] [Edge (Vertex 1) (Vertex 2)]
+            let graph = Graph [Vertex 1] [Edge (Vertex 1) (Vertex 2) 0]
             hasVertex (Vertex 2) graph `shouldBe` False
 
         it "hasVertex - exists" $ do
-            let graph = Graph [Vertex 1] [Edge (Vertex 1) (Vertex 2)]
+            let graph = Graph [Vertex 1] [Edge (Vertex 1) (Vertex 2) 0]
             hasVertex (Vertex 1) graph `shouldBe` True
 
         it "addVertex - does not yet exist" $ do
-            let isGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
-            let expectedGraph = Graph [Vertex 3, Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            let isGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
+            let expectedGraph = Graph [Vertex 3, Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
             addVertex (Vertex 3) isGraph `shouldBe` expectedGraph
 
         it "addVertex - exists" $ do
-            let isGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
-            let expectedGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            let isGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
+            let expectedGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
             addVertex (Vertex 1) isGraph `shouldBe` expectedGraph
             addVertex (Vertex 2) isGraph `shouldBe` expectedGraph
 
 
     describe "Edge" $ do
         it "hasEdge - does not yet exist" $ do
-            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
-            hasEdge (Edge (Vertex 3) (Vertex 2)) graph `shouldBe` False
+            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
+            hasEdge (Edge (Vertex 3) (Vertex 2) 0) graph `shouldBe` False
 
         it "hasEdge - exist" $ do
-            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
-            hasEdge (Edge (Vertex 1) (Vertex 2)) graph `shouldBe` True
+            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
+            hasEdge (Edge (Vertex 1) (Vertex 2) 0) graph `shouldBe` True
 
         it "hasEdge - exist, undirected" $ do
-            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
-            hasEdge (Edge (Vertex 2) (Vertex 1)) graph `shouldBe` True
+            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
+            hasEdge (Edge (Vertex 2) (Vertex 1) 0) graph `shouldBe` True
 
         it "addEdge - does not yet exist" $ do
             let isGraph = Graph [Vertex 1, Vertex 2] []
-            let expectedGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
-            addEdge (Edge (Vertex 1) (Vertex 2)) isGraph `shouldBe` expectedGraph
+            let expectedGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
+            addEdge (Edge (Vertex 1) (Vertex 2) 0) isGraph `shouldBe` expectedGraph
 
         it "addEdge - exists" $ do
-            let isGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
-            let expectedGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
-            addEdge (Edge (Vertex 1) (Vertex 2)) isGraph `shouldBe` expectedGraph
+            let isGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
+            let expectedGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
+            addEdge (Edge (Vertex 1) (Vertex 2) 0) isGraph `shouldBe` expectedGraph
 
         it "addEdge - exists, undirected" $ do
-            let isGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
-            let expectedGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
-            addEdge (Edge (Vertex 2) (Vertex 1)) isGraph `shouldBe` expectedGraph
+            let isGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
+            let expectedGraph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
+            addEdge (Edge (Vertex 2) (Vertex 1) 0) isGraph `shouldBe` expectedGraph
 
         it "adjacent - edge does not exist" $ do
-            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
             adjacent (Vertex 3) (Vertex 2) graph `shouldBe` False
 
         it "adjacent - exists" $ do
-            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
             adjacent (Vertex 1) (Vertex 2) graph `shouldBe` True
 
         it "adjacent - exists, undirected" $ do
-            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
             adjacent (Vertex 2) (Vertex 1) graph `shouldBe` True
 
     describe "neighbors" $ do
         it "vertex has no neighbors" $ do
-            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
             neighbors (Vertex 3) graph `shouldBe` []
 
         it "vertex has one neighbor" $ do
-            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2)]
+            let graph = Graph [Vertex 1, Vertex 2] [Edge (Vertex 1) (Vertex 2) 0]
             neighbors (Vertex 1) graph `shouldBe` [Vertex 2]
 
         it "vertex has neighbors" $ do
             let vertices = [Vertex 1, Vertex 2, Vertex 3, Vertex 4]
             let edges = [
-                            Edge (Vertex 1) (Vertex 2)
-                          , Edge (Vertex 2) (Vertex 3)
-                          , Edge (Vertex 3) (Vertex 4)
-                          , Edge (Vertex 4) (Vertex 1)
-                          , Edge (Vertex 4) (Vertex 2)
-                          , Edge (Vertex 1) (Vertex 3)
+                            Edge (Vertex 1) (Vertex 2) 0
+                          , Edge (Vertex 2) (Vertex 3) 0
+                          , Edge (Vertex 3) (Vertex 4) 0
+                          , Edge (Vertex 4) (Vertex 1) 0
+                          , Edge (Vertex 4) (Vertex 2) 0
+                          , Edge (Vertex 1) (Vertex 3) 0
                         ]
             let graph = Graph vertices edges
             neighbors (Vertex 1) graph `shouldBe` [Vertex 2, Vertex 4, Vertex 3]
