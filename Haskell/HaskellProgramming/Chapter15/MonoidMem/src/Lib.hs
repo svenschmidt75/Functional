8df9925f45ed9fdca033315d332fb0878666c565
Sven Schmidt <s.schmidt@questintegrity.com>
2016-10-15 08:22:51 -0600

    Exercise 15.8 completed

--- Haskell/HaskellProgramming/Chapter15/MonoidMem/src/Lib.hs
+++ Haskell/HaskellProgramming/Chapter15/MonoidMem/src/Lib.hs
@@ -9,11 +9,11 @@ import Data.Monoid hiding ((<>))
 -- SS on 10/15/2016: This looks like the State monad...
 newtype Mem s a = Mem { runMem :: s -> (a,s) }
 
-instance Semigroup (Mem s a) where
-    f1 <> f2 = Mem $ \s -> let (a1, s1) = runMem f1 s in
-                               (a1, s1)
+instance Semigroup a => Semigroup (Mem s a) where
+    f <> g = Mem $ \s -> let (a', s')  = runMem f s
+                             (a'', s'') = runMem g s'
+                           in (a' <> a'', s'')
 
-
-instance Monoid a => Monoid (Mem s a) where
-    mempty  = undefined
+instance (Semigroup a, Monoid a) => Monoid (Mem s a) where
+    mempty  = Mem $ \s -> (mempty, s)
     mappend = (<>)
--- Haskell/HaskellProgramming/Chapter15/MonoidMem/test/MonoidMemSpec.hs
+++ Haskell/HaskellProgramming/Chapter15/MonoidMem/test/MonoidMemSpec.hs
@@ -26,9 +26,23 @@ spec :: Spec
 spec = do
     describe "conidtions" $ do
         let f' = Mem $ \s -> ("hi", s + 1)
-        it "condition 1" $ do
+        it "right identity" $ do
             let result = runMem (f' <> mempty) 0
             result `shouldBe` ("hi",1)
+        it "left identity" $ do
+            let result = runMem (mempty <> f') 0
+            result `shouldBe` ("hi",1)
+        it "identity" $ do
+            let result = (runMem mempty 0 :: (String, Int))
+            result `shouldBe` ("",0)
+        it "right identity" $ do
+            let result = runMem (f' <> mempty) 0 == runMem f' 0
+            result `shouldBe` True
+        it "left identity" $ do
+            let result = runMem (mempty <> f') 0 == runMem f' 0
+            result `shouldBe` True
+
+
 
 -- I need to figure out how to generate functions in QuickCheck (CoArbitrary)
 {-
