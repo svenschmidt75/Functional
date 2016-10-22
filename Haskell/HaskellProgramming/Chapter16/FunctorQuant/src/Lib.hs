d053cf4c153bb50390c7962659ad81a98b75992b
Sven Schmidt <s.schmidt@questintegrity.com>
2016-10-22 08:02:44 -0600

    Functor Quant exercise

---
+++ Haskell/HaskellProgramming/Chapter16/FunctorQuant/src/Lib.hs
@@ -0,0 +1,13 @@
+module Lib
+    ( Quant (..)
+    ) where
+
+data Quant a b = Finance
+               | Desk a
+               | Bloor b
+    deriving (Eq, Show)
+
+instance Functor (Quant a) where
+    fmap _ Finance   = Finance
+    fmap _ (Desk a)  = Desk a
+    fmap f (Bloor b) = Bloor (f b)
