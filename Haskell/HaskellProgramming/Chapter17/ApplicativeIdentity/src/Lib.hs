2e819139e6bdbc9d9b4fbda725053f22c6ede188
Sven Schmidt <svenschmidt75@googlemail.com>
2016-10-29 09:23:32 -0600

    Applicative Identity

---
+++ Haskell/HaskellProgramming/Chapter17/ApplicativeIdentity/src/Lib.hs
@@ -0,0 +1,21 @@
+module Lib
+    ( Identity (..)
+    ) where
+
+newtype Identity a = Identity a
+    deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+    fmap f (Identity a) = Identity $ f a
+
+instance Applicative Identity where
+-- f ~ Identity
+-- Applicative f =>
+
+-- pure :: a -> f a
+-- pure :: a -> Identity a
+    pure = Identity
+
+-- (<*>) :: f (a -> b) -> f a -> f b
+-- (<*>) :: Identity (a -> b) -> Identity a -> Identity b
+    (<*>) (Identity f) (Identity a) = Identity $ f a
