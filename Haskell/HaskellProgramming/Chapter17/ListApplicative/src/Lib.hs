6963092314fe10b12ad4873980655b9dc2dbf671
Sven Schmidt <svenschmidt75@googlemail.com>
2016-11-24 09:06:23 -0700

    ZipList Applicative

--- Haskell/HaskellProgramming/Chapter17/ListApplicative/src/Lib.hs
+++ Haskell/HaskellProgramming/Chapter17/ListApplicative/src/Lib.hs
@@ -1,9 +1,5 @@
 module Lib
     ( List (..)
-    , applyOne
-    , concat'
-    , prepend
-    , flatten
     ) where
 
 data List a = Nil
@@ -15,7 +11,7 @@ instance Functor List where
     fmap f (Cons a as) = Cons (f a) (fmap f as)
 
 applyOne :: (a -> b) -> List a -> List b
-applyOne f Nil         = Nil
+applyOne _ Nil         = Nil
 applyOne f (Cons a as) = Cons (f a) (applyOne f as)
 
 -- prepend an item to a list
@@ -25,17 +21,11 @@ prepend a as  = Cons a as
 
 concat' :: List a -> List a -> List a
 concat' Nil xs         = xs
-concat' xs   Nil       = xs
-concat' (Cons a as) bs = concat' as (prepend a bs)
-
-flatten :: [List a] -> List a
-flatten [] = Nil
-flatten (x:y:xs) = concat' (concat' x y) (flatten xs)
-flatten (x:xs) = concat' x (flatten xs)
+concat' xs  Nil        = xs
+concat' (Cons a as) bs = prepend a $ concat' as bs
 
 instance Applicative List where
     pure a = Cons a Nil
-    (<*>) Nil _   = Nil
-    (<*>) _   Nil = Nil
-    (<*>) (Cons f Nil) xs = applyOne f xs
-    (<*>) (Cons f fs) xs = concat' (fs <*> xs) (applyOne f xs)
+    (<*>) Nil         _   = Nil
+    (<*>) _           Nil = Nil
+    (<*>) (Cons f fs) xs  = concat' (applyOne f xs) (fs <*> xs)
--- Haskell/HaskellProgramming/Chapter17/ZipListApplicative/test/ZipListApplicativeSpec.hs
+++ Haskell/HaskellProgramming/Chapter17/ZipListApplicative/test/ZipListApplicativeSpec.hs
@@ -1,6 +1,4 @@
-module ListApplicativeSpec (spec) where
-
-import Data.Semigroup
+module ZipListApplicativeSpec (spec) where
 
 import Test.Hspec
 import Test.Hspec.QuickCheck (prop)
@@ -10,8 +8,9 @@ import Test.QuickCheck (Property, quickCheck, (==>))
 import Test.QuickCheck.Monadic (assert, monadicIO, run)
 
 import Lib
-    ( List (..)
+    ( Identity (..)
     )
+
 
 functorIdentity :: (Functor f, Eq (f a)) => f a -> Bool
 functorIdentity f = fmap id f == f
@@ -23,7 +22,7 @@ functorCompose' :: (Eq (f c), Functor f) => f a -> Fun a b -> Fun b c -> Bool
 functorCompose' x (Fun _ f) (Fun _ g) = (fmap (g . f) x) == (fmap g . fmap f $ x)
 
 type IntToInt = Fun Int Int
-type IntFC = List Int -> IntToInt -> IntToInt -> Bool
+type IntFC = Identity Int -> IntToInt -> IntToInt -> Bool
 
 semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
 semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
@@ -34,30 +33,39 @@ monoidLeftIdentity a = (mempty <> a) == a
 monoidRightIdentity :: (Eq m, Semigroup m, Monoid m) => m -> Bool
 monoidRightIdentity a = (a <> mempty) == a
 
-instance Arbitrary a => Arbitrary (List a) where
+instance Arbitrary BoolDisj where
+    arbitrary = frequency [
+                            (1, return $ BoolDisj True)
+                          , (1, return $ BoolDisj False)
+                          ]
+
+instance Arbitrary a => Arbitrary (Identity a) where
     arbitrary = do
         a <- arbitrary
-        frequency [(1, return $ Cons a Nil)
-                 , (1, return $ Nil)]
+        return $ Identity a
 
 spec :: Spec
 spec = do
-    describe "List Functor Laws" $ do
+    describe "functor laws" $ do
         prop "identity" $ do
-            \x -> functorIdentity (x :: List Int)
+            \x -> functorIdentity (x :: Identity Int)
         prop "composition 1" $ do
             let li = functorCompose (+1) (*2)
-            \x -> li (x :: List Int)
+            \x -> li (x :: Identity Int)
         prop "composition 2" $ do
             functorCompose' :: IntFC
-    describe "Applicative Tests" $ do
-        it "test 1" $ do
-            let functions = Cons (+1) Nil
-            let values = Cons 1 Nil
-            let result = functions <*> values
-            result `shouldBe` (Cons 2 Nil)
-        it "test 2" $ do
-            let functions = Cons (+1) (Cons (*2) Nil)
-            let values = Cons 1 (Cons 2 Nil)
-            let result = functions <*> values
-            result `shouldBe` (Cons 2 (Cons 3 (Cons 2 (Cons 4 Nil))))
+    describe "conditions" $ do
+        it "condition 1" $ do
+            let f = Combine $ \n -> Sum (n + 1)
+            let result = unCombine (mappend f mempty) $ 1
+            result `shouldBe` Sum 2
+        prop "verify associativety" $ do
+            semigroupAssoc :: BoolConjAssoc
+
+prop5 :: Property
+prop5 = monadicIO $ do
+    let e = let ioi = readIO "1" :: IO Integer
+                changed = (fmap read $ fmap ("123" ++) (fmap show ioi)) :: IO Integer
+            in fmap (*3) changed
+    result <- run $ e
+    assert $ result == 3693
