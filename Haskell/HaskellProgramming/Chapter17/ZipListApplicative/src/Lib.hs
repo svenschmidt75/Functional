0d0e68f302758b7b6e366a4b316e5bca78020629
Sven Schmidt <svenschmidt75@googlemail.com>
2016-11-25 08:11:24 -0700

    applicative for ZipList'

--- Haskell/HaskellProgramming/Chapter17/ZipListApplicative/src/Lib.hs
+++ Haskell/HaskellProgramming/Chapter17/ZipListApplicative/src/Lib.hs
@@ -1,6 +1,9 @@
 module Lib
-    ( Identity (..)
+    ( List (..)
+    , ZipList' (..)
     ) where
+
+
 
 data List a = Nil
             | Cons a (List a)
@@ -16,15 +19,12 @@ instance Applicative List where
     pure = undefined
     (<*>) = undefined
 
+
+
+
+
 newtype ZipList' a = ZipList' (List a)
     deriving (Eq, Show)
-
-instance Eq a => EqProp (ZipList' a) where
-    xs =-= ys = xs' `eq` ys'
-      where xs' = let (ZipList' l) = xs
-                  in take' 3000 l
-    ys' = let (ZipList' l) = ys
-          in take' 3000 l
 
 instance Functor ZipList' where
     fmap f (ZipList' xs) = ZipList' $ fmap f xs
--- Haskell/HaskellProgramming/Chapter17/ZipListApplicative/test/ZipListApplicativeSpec.hs
+++ Haskell/HaskellProgramming/Chapter17/ZipListApplicative/test/ZipListApplicativeSpec.hs
@@ -2,70 +2,44 @@ module ZipListApplicativeSpec (spec) where
 
 import Test.Hspec
 import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck.Checkers
 import Test.QuickCheck
 import Test.QuickCheck.Function
 import Test.QuickCheck (Property, quickCheck, (==>))
 import Test.QuickCheck.Monadic (assert, monadicIO, run)
 
 import Lib
-    ( Identity (..)
+    ( List (..)
+    , ZipList' (..)
     )
 
 
-functorIdentity :: (Functor f, Eq (f a)) => f a -> Bool
-functorIdentity f = fmap id f == f
+take' :: Int -> List a -> List a
+take' = undefined
 
-functorCompose :: (Eq (f c), Functor f) => (a -> b) -> (b -> c) -> f a -> Bool
-functorCompose f g x = (fmap g (fmap f x)) == (fmap (g . f) x)
+instance Eq a => EqProp (ZipList' a) where
+    xs =-= ys = xs' `eq` ys'
+      where xs' = let (ZipList' l) = xs
+                  in take' 3000 l
+            ys' = let (ZipList' l) = ys
+                  in take' 3000 l
 
-functorCompose' :: (Eq (f c), Functor f) => f a -> Fun a b -> Fun b c -> Bool
-functorCompose' x (Fun _ f) (Fun _ g) = (fmap (g . f) x) == (fmap g . fmap f $ x)
-
-type IntToInt = Fun Int Int
-type IntFC = Identity Int -> IntToInt -> IntToInt -> Bool
-
-semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
-semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
-
-monoidLeftIdentity :: (Eq m, Semigroup m, Monoid m) => m -> Bool
-monoidLeftIdentity a = (mempty <> a) == a
-
-monoidRightIdentity :: (Eq m, Semigroup m, Monoid m) => m -> Bool
-monoidRightIdentity a = (a <> mempty) == a
-
-instance Arbitrary BoolDisj where
-    arbitrary = frequency [
-                            (1, return $ BoolDisj True)
-                          , (1, return $ BoolDisj False)
-                          ]
-
-instance Arbitrary a => Arbitrary (Identity a) where
+instance Arbitrary a => Arbitrary (List a) where
     arbitrary = do
         a <- arbitrary
-        return $ Identity a
+        frequency [(1, return $ Cons a Nil)
+                 , (1, return $ Nil)]
 
 spec :: Spec
 spec = do
-    describe "functor laws" $ do
-        prop "identity" $ do
-            \x -> functorIdentity (x :: Identity Int)
-        prop "composition 1" $ do
-            let li = functorCompose (+1) (*2)
-            \x -> li (x :: Identity Int)
-        prop "composition 2" $ do
-            functorCompose' :: IntFC
-    describe "conditions" $ do
-        it "condition 1" $ do
-            let f = Combine $ \n -> Sum (n + 1)
-            let result = unCombine (mappend f mempty) $ 1
-            result `shouldBe` Sum 2
-        prop "verify associativety" $ do
-            semigroupAssoc :: BoolConjAssoc
-
-prop5 :: Property
-prop5 = monadicIO $ do
-    let e = let ioi = readIO "1" :: IO Integer
-                changed = (fmap read $ fmap ("123" ++) (fmap show ioi)) :: IO Integer
-            in fmap (*3) changed
-    result <- run $ e
-    assert $ result == 3693
+    describe "List Applicative Tests" $ do
+        it "test 1" $ do
+            let functions = Cons (+1) Nil
+            let values = Cons 1 Nil
+            let result = functions <*> values
+            result `shouldBe` (Cons 2 Nil)
+        it "test 2" $ do
+            let functions = Cons (+1) (Cons (*2) Nil)
+            let values = Cons 1 (Cons 2 Nil)
+            let result = functions <*> values
+            result `shouldBe` (Cons 2 (Cons 3 (Cons 2 (Cons 4 Nil))))
