1fe9a5ea7e69b370acc728edf8bb80b804659833
Sven Schmidt <svenschmidt75@googlemail.com>
2016-11-27 09:52:48 -0700

    minor changes

--- Haskell/HaskellProgramming/Chapter15/SemigroupCombine/test/LibSpec.hs
+++ Haskell/HaskellProgramming/Chapter15/SemigroupCombine/test/LibSpec.hs
@@ -16,6 +16,8 @@ import Lib
 semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
 semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
 
+type CombineType = Combine Int Int
+type CombineAssoc = CombineType -> CombineType -> CombineType -> Bool
 
 
 -- `main` is here so that this module can be run from GHCi on its own.  It is
@@ -36,9 +38,7 @@ spec = do
             (unCombine (f <> f) $ 1) `shouldBe` (Sum 4)
         it "4th condition" $ do
             (unCombine (g <> f) $ 1) `shouldBe` (Sum 2)
-
-{-
     describe "Semigroup Associativity" $ do
         prop "verify associativety" $
-            \a b c -> semigroupAssoc (a :: BoolConj) (b :: BoolConj) (c :: BoolConj)
--}
+            \x y z -> semigroupAssoc (x :: CombineType) (y :: CombineType) (z :: CombineType)
+--            semigroupAssoc :: CombineAssoc
