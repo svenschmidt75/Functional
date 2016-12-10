ba19ef1a79bb9d94e5870ad10b0c89d96ff3d877
Sven Schmidt <svenschmidt75@googlemail.com>
2016-12-10 08:35:15 -0700

    more Chapter 20 exercises

--- Haskell/HaskellProgramming/Chapter20/LibraryFunctions/src/Lib.hs
+++ Haskell/HaskellProgramming/Chapter20/LibraryFunctions/src/Lib.hs
@@ -1,7 +1,11 @@
 module Lib where
 
---foldMap :: Data.Monoid.Monoid m => (a -> m) -> t a -> m
+import Prelude hiding (foldMap)
+import Data.Monoid ((<>))
+
+-- foldMap :: Data.Monoid.Monoid m => (a -> m) -> t a -> m
 -- foldr :: (a -> b -> b) -> b -> t a -> b
+-- fold :: Data.Monoid.Monoid m => t m -> m
 
 sum :: (Foldable t, Num a) => t a -> a
 sum x = foldr (+) 0 x
@@ -15,22 +19,35 @@ elem x xs = foldr (\a b -> a == x || b) False xs
 minimum :: (Foldable t, Ord a) => t a -> Maybe a
 minimum xs = foldr min' Nothing xs
              where
-               min' a Nothing = Just a
-               min' a (Just b) = case compare a b of
-                                   LT -> Just a
-                                   EQ -> Just a
-                                   GT -> Just b
+               min' a Nothing  = Just a
+               min' a (Just b) = if compare a b == GT
+                                 then Just b
+                                 else Just a
 
-{-
-4. minimum :: (Foldable t, Ord a) => t a -> Maybe a
-5. maximum :: (Foldable t, Ord a) => t a -> Maybe a
-6. null :: (Foldable t) => t a -> Bool
-7. length :: (Foldable t) => t a -> Int
-8. Some say this is all Foldable amounts to.
+maximum :: (Foldable t, Ord a) => t a -> Maybe a
+maximum xs = foldr max' Nothing xs
+             where
+               max' a Nothing  = Just a
+               max' a (Just b) = if compare a b == GT
+                                 then Just a
+                                 else Just b
+
+null :: (Foldable t) => t a -> Bool
+null xs = isEmpty $ foldr (:) [] xs
+        where
+          isEmpty [] = True
+          isEmpty _  = False
+
+length :: (Foldable t) => t a -> Int
+length xs = foldr (\_ b -> b + 1) 0 xs
+
 toList :: (Foldable t) => t a -> [a]
-9. Hint: use foldMap.
--- | Combine the elements of a structure using a monoid.
+toList xs = foldr (:) [] xs
+
 fold :: (Foldable t, Monoid m) => t m -> m
-10. Define foldMap in terms of foldr.
+-- id works here because the argument of fold contains monoids,
+-- so we don't have to map them to monoids, but to itself instead!
+fold xs = foldMap id xs
+
 foldMap :: (Foldable t, Monoid m) => (a -> m) -> t a -> m
--}
+foldMap f xs = foldr (\a b -> f a <> b) mempty xs
--- Haskell/HaskellProgramming/Chapter20/LibraryFunctions/test/LibraryFunctionsSpec.hs
+++ Haskell/HaskellProgramming/Chapter20/LibraryFunctions/test/LibraryFunctionsSpec.hs
@@ -2,6 +2,7 @@ module LibraryFunctionsSpec (spec) where
 
 import Test.Hspec
 import Test.Hspec.QuickCheck
+import Data.Monoid
 
 import Lib
 
@@ -29,6 +30,39 @@ spec = do
         it "non-empty" $ do
             Lib.minimum [3, 7, 1, 9, 4, 1, 2, 3, 4, -1 :: Int] `shouldBe` Just (-1)
         prop "prop test" prop_minimum
+    describe "maximum" $ do
+        it "empty" $ do
+            Lib.maximum [] `shouldBe` (Nothing :: Maybe Int)
+        it "non-empty" $ do
+            Lib.maximum [3, 7, 1, 9, 4, 1, 2, 3, 4, -1 :: Int] `shouldBe` Just 9
+        prop "prop test" prop_maximum
+    describe "null" $ do
+        it "empty" $ do
+            Lib.null [] `shouldBe` True
+        it "non-empty" $ do
+            Lib.null [3, -1 :: Int] `shouldBe` False
+        prop "prop test" prop_null
+    describe "length" $ do
+        it "empty" $ do
+            Lib.length [] `shouldBe` 0
+        it "non-empty" $ do
+            Lib.length [3, -1 :: Int] `shouldBe` 2
+        prop "prop test" prop_length
+    describe "toList" $ do
+        it "Maybe" $ do
+            Lib.toList (Just 1 :: Maybe Int) `shouldBe` [1]
+        it "tuple" $ do
+            Lib.toList (1 :: Int, 2 :: Int) `shouldBe` [2 :: Int]
+    describe "fold" $ do
+        it "Sum" $ do
+            Lib.fold [1, 2 :: Sum Int] `shouldBe` Sum 3
+        it "Product" $ do
+            Lib.fold [1, 8, 3 :: Product Int] `shouldBe` Product 24
+    describe "foldMap" $ do
+        it "Sum" $ do
+            Lib.foldMap Sum [1, 2 :: Int] `shouldBe` Sum 3
+        it "Product" $ do
+            Lib.foldMap Product [1, 8, 3 :: Int] `shouldBe` Product 24
 
 prop_sum :: [Int] -> Bool
 prop_sum xs = Lib.sum xs == Prelude.sum xs
@@ -43,3 +77,14 @@ prop_minimum :: [Int] -> Bool
 prop_minimum xs = case Lib.minimum xs of
                     Just a  -> a == Prelude.minimum xs
                     Nothing -> True
+
+prop_maximum :: [Int] -> Bool
+prop_maximum xs = case Lib.maximum xs of
+                    Just a  -> a == Prelude.maximum xs
+                    Nothing -> True
+
+prop_null :: [Int] -> Bool
+prop_null xs = Lib.null xs == Prelude.null xs
+
+prop_length :: [Int] -> Bool
+prop_length xs = Lib.length xs == Prelude.length xs
