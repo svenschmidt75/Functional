d03b3a5d10fc08366279a65827adacfcad594bd9
Sven Schmidt <svenschmidt75@googlemail.com>
2016-12-10 07:49:20 -0700

    more Chapter 2- exercises

--- Haskell/HaskellProgramming/Chapter20/LibraryFunctions/src/Lib.hs
+++ Haskell/HaskellProgramming/Chapter20/LibraryFunctions/src/Lib.hs
@@ -12,6 +12,15 @@ product x = foldr (*) 1 x
 elem :: (Foldable t, Eq a) => a -> t a -> Bool
 elem x xs = foldr (\a b -> a == x || b) False xs
 
+minimum :: (Foldable t, Ord a) => t a -> Maybe a
+minimum xs = foldr min' Nothing xs
+             where
+               min' a Nothing = Just a
+               min' a (Just b) = case compare a b of
+                                   LT -> Just a
+                                   EQ -> Just a
+                                   GT -> Just b
+
 {-
 4. minimum :: (Foldable t, Ord a) => t a -> Maybe a
 5. maximum :: (Foldable t, Ord a) => t a -> Maybe a
--- Haskell/HaskellProgramming/Chapter20/LibraryFunctions/test/LibraryFunctionsSpec.hs
+++ Haskell/HaskellProgramming/Chapter20/LibraryFunctions/test/LibraryFunctionsSpec.hs
@@ -1,11 +1,7 @@
 module LibraryFunctionsSpec (spec) where
 
-import Prelude hiding (
-                        sum
-                      , product
-                      , elem
-                      )
 import Test.Hspec
+import Test.Hspec.QuickCheck
 
 import Lib
 
@@ -14,13 +10,36 @@ spec = do
     describe "sum" $ do
         it "test 1" $ do
             let expected = 10
-            sum [1, 2, 3, 4 :: Int] `shouldBe` expected
+            Lib.sum [1, 2, 3, 4 :: Int] `shouldBe` expected
+        prop "prop test" prop_sum
     describe "product" $ do
         it "test 1" $ do
             let expected = 24
-            product [1, 2, 3, 4 :: Int] `shouldBe` expected
+            Lib.product [1, 2, 3, 4 :: Int] `shouldBe` expected
+        prop "prop test" prop_product
     describe "elem" $ do
         it "exists" $ do
-            elem 1 [1, 2, 3, 4 :: Int] `shouldBe` True
+            Lib.elem 1 [1, 2, 3, 4 :: Int] `shouldBe` True
         it "does not exists" $ do
-            elem 5 [1, 2, 3, 4 :: Int] `shouldBe` False
+            Lib.elem 5 [1, 2, 3, 4 :: Int] `shouldBe` False
+        prop "prop test" prop_elem
+    describe "minimum" $ do
+        it "empty" $ do
+            Lib.minimum [] `shouldBe` (Nothing :: Maybe Int)
+        it "non-empty" $ do
+            Lib.minimum [3, 7, 1, 9, 4, 1, 2, 3, 4, -1 :: Int] `shouldBe` Just (-1)
+        prop "prop test" prop_minimum
+
+prop_sum :: [Int] -> Bool
+prop_sum xs = Lib.sum xs == Prelude.sum xs
+
+prop_product :: [Int] -> Bool
+prop_product xs = Lib.product xs == Prelude.product xs
+
+prop_elem :: Int -> [Int] -> Bool
+prop_elem x xs = Lib.elem x xs == Prelude.elem x xs
+
+prop_minimum :: [Int] -> Bool
+prop_minimum xs = case Lib.minimum xs of
+                    Just a  -> a == Prelude.minimum xs
+                    Nothing -> True
