331291955f749649d9f6c774fcbf557ded1bb7b8
Sven Schmidt <svenschmidt75@googlemail.com>
2016-12-11 16:32:40 -0700

    Either in C#

---
+++ MonadsC#/SelectMany/Either/Either.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Either
+{
+    public static class Either
+    {
+        public static Either<T1, T2> Ok<T1, T2>(T1 value)
+        {
+            return new Either<T1, T2>(value);
+        }
+
+        public static Either<T1, T2> Error<T1, T2>(T2 error)
+        {
+            return new Either<T1, T2>(error);
+        }
+
+        public static Either<TResult2, TError> Bind<TResult1, TError, TResult2>(this Either<TResult1, TError> ma, Func<TResult1, Either<TResult2, TError>> func)
+        {
+            if (ma.What == Either<TResult1, TError>.Type.Error)
+            {
+                return Either.Error<TResult2, TError>(ma.Error);
+            }
+            return func(ma.Value);
+        }
+    }
+
+    public class Either<T1, T2>
+    {
+        public Either(T1 value)
+        {
+            What = Type.Ok;
+            Value = value;
+        }
+
+        public Either(T2 error)
+        {
+            What = Type.Error;
+            Error = error;
+        }
+
+        public enum Type
+        {
+            Error,
+            Ok
+        };
+
+        public T1 Value { get; private set; }
+
+        public T2 Error { get; private set; }
+
+        public Type What { get; private set; }
+    }
+}
---
+++ MonadsC#/SelectMany/Either/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Either
+{
+    internal class Program
+    {
+        public Either<int, string> Foo(int arg)
+        {
+            return Either.Ok<int, string>(arg);
+        }
+
+        public Either<int, string> Bar(int arg)
+        {
+            return Either.Ok<int, string>(arg);
+        }
+
+        public Either<int, string> Fail(int arg)
+        {
+            return Either.Error<int, string>("Failed");
+        }
+
+        public static void Main(string[] args)
+        {
+            var p = new Program();
+            var result1 = p.Foo(1);
+            var result2 = result1.Bind(value => p.Bar(value));
+            Console.WriteLine($"Result: {result2.What}");
+
+            var result3 = result2.Bind(value => p.Fail(value));
+            Console.WriteLine($"Result: {result3.What}");
+        }
+    }
+}
