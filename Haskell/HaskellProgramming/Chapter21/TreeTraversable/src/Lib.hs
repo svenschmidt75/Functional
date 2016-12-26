bba33d887181d91a4604b822d7b9581af0fc1db6
Sven Schmidt <svenschmidt75@googlemail.com>
2016-12-26 09:07:50 -0700

    minor changes

--- Haskell/HaskellProgramming/Chapter21/TreeTraversable/src/Lib.hs
+++ Haskell/HaskellProgramming/Chapter21/TreeTraversable/src/Lib.hs
@@ -2,8 +2,6 @@ module Lib
     ( Tree (..)
     ) where
 
-import Data.Semigroup
-
 
 data Tree a = Empty
             | Leaf a
@@ -16,20 +14,29 @@ instance Functor Tree where
     fmap f (Leaf a)     = Leaf $ f a
     fmap f (Node l a r) = Node (f <$> l) (f a) (f <$> r)
 
--- foldMap is a bit easier and looks more natural,
--- but you can do foldr too for extra credit.
+unfold' :: Monoid m => Tree m -> m
+unfold' Empty        = mempty
+unfold' (Leaf a)     = a
+unfold' (Node l a r) = unfold' l `mappend` a `mappend` unfold' r
+
 instance Foldable Tree where
 --  foldr :: (a -> b -> b) -> b -> t     a -> b
 --  foldMap :: Monoid m => (a -> m) -> t    a -> m
 --  foldMap :: Monoid m => (a -> m) -> Tree a -> m
-    foldMap _ Empty        = mempty
-    foldMap f (Leaf a)     = f a
-    foldMap f (Node l a r) = foldMap f l `mappend` f a `mappend` foldMap f r
---    foldMap f a =
+    foldMap f t = unfold' $ f <$> t
+-- or
+--    foldMap _ Empty        = mempty
+--    foldMap f (Leaf a)     = f a
+--    foldMap f (Node l a r) = foldMap f l `mappend` f a `mappend` foldMap f r
 
 instance Traversable Tree where
 -- traverse :: Applicative f => (a -> f b) -> t    a -> f (t    b)
 -- traverse :: Applicative f => (a -> f b) -> Tree a -> f (Tree b)
-    traverse f Empty = pure Empty
-    traverse f (Leaf a) = Leaf <$> f a
-    traverse f (Node l a r) = Node <$> traverse f l <*> (f a) <*> (traverse f r)
+
+-- For traverse, think Functor but with some Functor thrown in.
+    traverse _ Empty        = pure Empty
+    traverse f (Leaf a)     = Leaf <$> f a
+    traverse f (Node l a r) = Node <$> traverse f l <*> f a <*> traverse f r
+-- or
+    -- traverse f t = unfoldTraverse $ f <$> t :: Tree (f b)
+    -- unfoldTraverse :: Applicative f => Tree (f b) -> f (Tree b) - or sequenceA
