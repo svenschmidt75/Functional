77cf5216a01634eaf2c867da1d4b40b25acc294c
Sven Schmidt <svenschmidt75@googlemail.com>
2017-01-14 08:43:26 -0700

    Chapter 24: Phone number exercise

--- Haskell/HaskellProgramming/Chapter24/PhoneNumbers/src/Lib.hs
+++ Haskell/HaskellProgramming/Chapter24/PhoneNumbers/src/Lib.hs
@@ -1,11 +1,11 @@
 module Lib
     ( PhoneNumber (..)
+    , parsePhone
     ) where
 
-import Data.Maybe
+import Data.Char
 import Control.Applicative
 import Text.Trifecta
-import Text.Parser.LookAhead
 
 
 type NumberingPlanArea = Int -- aka area code
@@ -15,5 +15,44 @@ type LineNumber = Int
 data PhoneNumber = PhoneNumber NumberingPlanArea Exchange LineNumber
     deriving (Eq, Show)
 
+parseCountryCode :: Parser (Maybe ())
+parseCountryCode = do
+    optional $ try $ do
+        _ <- char '1' >> char '-'
+        return ()
+
+parse3Digits :: Parser Int
+parse3Digits = do
+    d1 <- digit
+    d2 <- digit
+    d3 <- digit
+    return $ 100 * (digitToInt d1) + 10 * (digitToInt d2) + (digitToInt d3)
+
+parse4Digits :: Parser Int
+parse4Digits = do
+    d1 <- digit
+    d2 <- digit
+    d3 <- digit
+    d4 <- digit
+    return $ 1000 * (digitToInt d1) + 100 * (digitToInt d2) + 10 * (digitToInt d3) + (digitToInt d4)
+
+parseNPA :: Parser NumberingPlanArea
+parseNPA =  parse3Digits
+        <|> parens parse3Digits
+
+parseExchange :: Parser Exchange
+parseExchange = do
+    whiteSpace
+    parse3Digits <|> (char '-' >> parse3Digits)
+
+parseLineNumber :: Parser LineNumber
+parseLineNumber = do
+    parse4Digits <|> (char '-' >> parse4Digits)
+
 parsePhone :: Parser PhoneNumber
-parsePhone = undefined
+parsePhone = do
+    _ <- parseCountryCode
+    npa <- parseNPA
+    exg <- parseExchange
+    ln <- parseLineNumber
+    return $ PhoneNumber npa exg ln
--- Haskell/HaskellProgramming/Chapter24/PhoneNumbers/test/PhoneNumbersSpec.hs
+++ Haskell/HaskellProgramming/Chapter24/PhoneNumbers/test/PhoneNumbersSpec.hs
@@ -5,46 +5,43 @@ import Test.Hspec
 import qualified Text.Trifecta as TF
 
 import Lib
-    ( SemVer (..)
-    , NumberOrString (..)
-    , parseSemVer
+    ( PhoneNumber (..)
+    , parsePhone
     )
 
 
 spec :: Spec
 spec = do
-    describe "SemVerParser" $ do
-        it "Test 1 - malformed version" $ do
-            let result = TF.parseString parseSemVer mempty "2.11"
+    describe "PhoneNumber Parser" $ do
+        it "Test 1" $ do
+            let result = TF.parseString parsePhone mempty "123-456-7890"
             -- TF.Result does not have an eq instance, so need to
             -- manually unpack
             case result of
-                TF.Success _-> "Parse should have failed on invalid version" `shouldBe` []
+                TF.Success pn-> pn `shouldBe` (PhoneNumber 123 456 7890)
                 -- fail this test...
-                TF.Failure _ -> False `shouldBe` False
+                TF.Failure err   -> show err   `shouldBe` "False"
         it "Test 2" $ do
-            let result = TF.parseString parseSemVer mempty "2.1.1"
+            let result = TF.parseString parsePhone mempty "1234567890"
             -- TF.Result does not have an eq instance, so need to
             -- manually unpack
             case result of
-                TF.Success semVer-> semVer `shouldBe` (SemVer 2 1 1 [] [])
+                TF.Success semVer-> semVer `shouldBe` (PhoneNumber 123 456 7890)
                 -- fail this test...
                 TF.Failure err   -> show err   `shouldBe` "False"
         it "Test 3" $ do
-            let result = TF.parseString parseSemVer mempty "1.0.0-x.7.z.92"
+            let result = TF.parseString parsePhone mempty "(123) 456-7890"
             -- TF.Result does not have an eq instance, so need to
             -- manually unpack
             case result of
-                TF.Success semVer-> semVer `shouldBe` (SemVer 1 0 0 [NOSS "x", NOSI 7, NOSS "z", NOSI 92] [])
+                TF.Success pn-> pn `shouldBe` (PhoneNumber 123 456 7890)
                 -- fail this test...
                 TF.Failure err   -> show err   `shouldBe` "False"
         it "Test 4" $ do
-            let result = TF.parseString parseSemVer mempty "1.0.0-beta+exp.sha.5114f85"
+            let result = TF.parseString parsePhone mempty "1-123-456-7890"
             -- TF.Result does not have an eq instance, so need to
             -- manually unpack
             case result of
-                TF.Success semVer-> semVer `shouldBe` (SemVer 1 0 0 [NOSS "beta"] [NOSS "exp", NOSS "sha", NOSS "5114f85"])
+                TF.Success pn-> pn `shouldBe` (PhoneNumber 123 456 7890)
                 -- fail this test...
                 TF.Failure err   -> show err   `shouldBe` "False"
-        it "Test 5" $ do
-            SemVer 2 1 1 [] [] > SemVer 2 1 0 [] [] `shouldBe` True
