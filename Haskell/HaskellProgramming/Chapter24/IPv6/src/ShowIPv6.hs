4ad55f3cf856a50cf47fa7ab4aabb71755e9825b
Sven Schmidt <svenschmidt75@googlemail.com>
2017-02-19 08:41:22 -0700

    cleanup

--- Haskell/HaskellProgramming/Chapter24/IPv6/src/ShowIPv6.hs
+++ Haskell/HaskellProgramming/Chapter24/IPv6/src/ShowIPv6.hs
@@ -8,7 +8,7 @@ import Data.Bits
 import qualified Text.Printf as TP
 
 
--- Group by consecutive index in list of tuples.
+-- Group elements by consecutive index in list of tuples.
 --  input: [(1,'0'),(2,'0'),(3,'0'),(4,'0'),(6,'0')]
 -- output: [[(1,'0'),(2,'0'),(3,'0'),(4,'0')],[(6,'0')]]
 groupByConsecutive :: [(Int, a)] -> [[(Int, a)]]
@@ -22,6 +22,8 @@ groupByConsecutive = groupByConsecutive' []
         groupByConsecutive' accum [x]      = [accum ++ [x]]
         groupByConsecutive' _     []       = [[]]
 
+-- Find indices of longest consecutive list of 0 in a list of numbers.
+-- Must be at least two 0 next to one another.
 --  input: [65152,0,0,0,514,46079,65054,33577]
 -- output: [1,2,3]
 getLongestZeroRange :: [Word16] -> [Int]
@@ -33,35 +35,41 @@ getLongestZeroRange input =
                 p5 = if length p4 < 2 then [] else p4
             in map fst p5
 
-decompose :: Word64 -> [Word16]
-decompose w64 = let w16_1 = fromInteger (toInteger w64 .&. 0xFFFF)
-                    w16_2 = fromInteger (toInteger w64 `shiftR` 16) .&. 0xFFFF
-                    w16_3 = fromInteger (toInteger w64 `shiftR` 32) .&. 0xFFFF
-                    w16_4 = fromInteger (toInteger w64 `shiftR` 48) .&. 0xFFFF
-                in [w16_4, w16_3, w16_2, w16_1]
+--  input: Word64 = [63..48 47..32 31..16 15..0]
+-- output: [63..48, 47..32, 31..16, 15..0]
+decompose64 :: Word64 -> [Word16]
+decompose64 w64 = let w16_1 = fromInteger (toInteger w64 .&. 0xFFFF)
+                      w16_2 = fromInteger (toInteger w64 `shiftR` 16) .&. 0xFFFF
+                      w16_3 = fromInteger (toInteger w64 `shiftR` 32) .&. 0xFFFF
+                      w16_4 = fromInteger (toInteger w64 `shiftR` 48) .&. 0xFFFF
+                  in [w16_4, w16_3, w16_2, w16_1]
 
-decomposeQuad :: Word64 -> Word64 -> [Word16]
-decomposeQuad hw lw = concat [decompose hw, decompose lw]
+decompose128 :: Word64 -> Word64 -> [Word16]
+decompose128 hw lw = concat [decompose64 hw, decompose64 lw]
 
 removeLeadingZeros :: [Word16] -> [Word16]
 removeLeadingZeros = dropWhile (== 0)
 
-intersperseQuad :: [Word16] -> [Int] -> [String]
-intersperseQuad ws [] = map f [0..length ws - 1]
+intersperse128 :: [Word16] -> [Int] -> [String]
+intersperse128 ws [] = map f [0..length ws - 1]
     where
         f idx
-          | idx == 0         = TP.printf "%X" (ws !! idx)
-          | otherwise        = ":" ++ TP.printf "%X" (ws !! idx)
-intersperseQuad ws idxs = map f [0..length ws - 1]
+          | idx == 0         = bitGroup
+          | otherwise        = ":" ++ bitGroup
+          where
+              bitGroup = TP.printf "%X" (ws !! idx)
+intersperse128 ws idxs = map f [0..length ws - 1]
     where
         f idx
           | idx == last idxs && idx == length ws - 1 = ":"
           | idx >= head idxs && idx < last idxs      = ""
           | idx == last idxs                         = ":"
-          | idx == length ws - 1                     = TP.printf "%X" (ws !! idx)
-          | otherwise                                = TP.printf "%X" (ws !! idx) ++ ":"
+          | idx == length ws - 1                     = bitGroup
+          | otherwise                                = bitGroup ++ ":"
+          where
+              bitGroup = TP.printf "%X" (ws !! idx)
 
 showQuad :: Word64 -> Word64 -> String
-showQuad hw lw = let r1 = removeLeadingZeros $ decomposeQuad hw lw
-                     r2 = intersperseQuad r1 (getLongestZeroRange r1)
+showQuad hw lw = let r1 = removeLeadingZeros $ decompose128 hw lw
+                     r2 = intersperse128 r1 (getLongestZeroRange r1)
                  in concat r2
