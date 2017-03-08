a6c3e26e516a512f393d011ab7adb1a6fbf0f0d1
Sven Schmidt <svenschmidt75@googlemail.com>
2017-03-07 17:33:53 -0700

    Julie Moronuki: Applicative and Monadic Parsing - λC Winter Retreat 2017

---
+++ Haskell/Playground/optex/src/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import Options.Applicative as Opt
+import Data.Semigroup ((<>))
+import Data.Char
+
+data Welcome = Welcome { name :: String }
+
+runWithOptions :: Welcome -> IO ()
+runWithOptions opts = putStrLn ("Enjoy the snow, " ++ name opts ++ "!")
+
+main :: IO ()
+main = execParser opts >>= runWithOptions
+    -- execParser :: ParserInfo a -> IO a
+    where
+        parser = Welcome <$> argument str (metavar "NAME")
+        opts   = info parser mempty
+    -- info :: Parser a -> InfoMod a -> ParserInfo a
