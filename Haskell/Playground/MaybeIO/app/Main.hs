f3f808baba14e58acd982f2a646852f82e106276
Sven Schmidt <svenschmidt75@googlemail.com>
2017-03-26 11:31:18 -0600

    minor changes

--- Haskell/Playground/MaybeIO/app/Main.hs
+++ Haskell/Playground/MaybeIO/app/Main.hs
@@ -4,49 +4,53 @@ import Lib ( MaybeIO (..)
            , liftIO
            , liftMaybe)
 
-
--- f :: IO (Maybe Int)
--- f = do
---     putStrLn "Enter a number: "
---     a <- read <$> getLine :: IO Int
---     putStrLn ("The number you entered is " ++ show a)
---     let mb = if a < 10 then
---                  Just a
---              else
---                  Nothing
---     mb >>= \value -> do
---         putStrLn ("Number: " ++ show value)
---         return value
-
 {-
-    a <- read <$> getLine :: IO Int
+    Combining IO with Maybe.
+    Because of the type signature of
+    (>>=) :: Monad m => m a -> (a -> m b) -> m b,
+    combining two monads like IO and Maybe is difficult.
+    Here is code that will not work:
+    main :: IO ()
+    main = do
+        resultIO <- runMaybeIO f
+        resultMaybe <- resultIO
+        print $ "Result: " ++ show resultMaube
+    To see why, let us remove the syntactic sugar for <-:
 
-    read <$> getLine >>= \a -> return b
+    runMaybeIO f >>= \resultIO ->
+        resultIO >>= \resultMaybe ->
+            print $ "Result: " ++ show resultMaube
 
-    where:
+    Now, the type signature of the 1st >>= is
+    (>>=) :: IO (Maybe Int) -> (Maybe Int -> IO (Maybe Int)) -> IO (Maybe Int)
+    and that of the second is
+    (>>=) ::Maybe Int -> (Int -> Maybe Int) -> Maybe Int
+    To see the types involved, it is easier to do it inside-out.
+    The inner function returns IO () (print), hence its type is
 
-    (>>=) :: IO Int -> (Int -> IO b) -> IO b
+    :: Int -> IO ()
+    \resultMaybe ->print $ "Result: " ++ show resultMaube
 
-    want:
-        (>>=) :: MaybeIO Int -> (Int -> MaybeIO b) -> MaybeIO b
-
-    Need to convert: IO a -> MaybeIO a...
+    but it should return Maybe Int.
+    As we can see, this is impossible due to the IO, which we
+    cannot get rid of. One solution is to combine Maybe with IO
+    such that IO is outer-most.
+    This is what MaybeIO does.
 -}
 
 f :: MaybeIO Int
 f = do
         liftIO $ putStrLn "Enter a number: "
-        a <- liftIO $ (read <$> getLine :: IO Int)
-        return a
-        -- liftIO $ putStrLn ("The number you entered is " ++ show a)
-        -- let mb = if a < 10 then
-        --              Just a
-        --          else
-        --              Nothing
-        -- liftIO $ putStrLn ("Number: " ++ show mb)
-        -- return mb
+        a <- liftIO $ read <$> getLine
+        liftIO $ putStrLn ("The number you entered is " ++ show a)
+        let mb = if a < 10 then
+                     Just a
+                 else
+                     Nothing
+        liftIO $ putStrLn ("Number: " ++ show mb)
+        liftMaybe mb
 
 main :: IO ()
 main = do
-    result <- runMaybeIO f
-    print $ "Result: " ++ show result
+    resultIO <- runMaybeIO f
+    print $ "Result: " ++ show resultIO
