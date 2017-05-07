c4bd8c02f3e8671ac08af12bb07e33f688eee156
Sven Schmidt <svenschmidt75@googlemail.com>
2017-05-07 08:50:18 -0600

    working EitherT monad transformer with checkers

--- Haskell/HaskellProgramming/Chapter26/EitherT/src/Lib.hs
+++ Haskell/HaskellProgramming/Chapter26/EitherT/src/Lib.hs
@@ -1,6 +1,4 @@
 {-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE UndecidableInstances #-}
 module Lib
     ( MyEitherT (..)
@@ -9,6 +7,27 @@ module Lib
 
 newtype MyEitherT a m b = MyEitherT { runMyEitherT :: m (Either a b) }
 
+{-
+This code,
+instance (Eq (m (Either a b))) => Eq (MyEitherT a m b) where
+
+triggers error
+
+    • The constraint ‘Eq (m (Either a b))’
+        is no smaller than the instance head
+      (Use UndecidableInstances to permit this)
+    • In the instance declaration for ‘Eq (MyEitherT a m b)’
+
+
+This is because teh compiler tries to make sure that
+Eq (MyEitherT a m b) makes sense, which means that each
+resolution step peels off one type constructor.
+Here, the contraint is the definition of MyEitherT itself,
+so it does not allow to remove one layer of constructor.
+There are tricks to make this work, dor example by introducing
+a newtype, see http://stackoverflow.com/questions/17863332/what-contraint-is-no-smaller-than-the-instance-head-means-and-how-to-solve-it
+but here we just go with the UndecidableInstances extension...
+-}
 instance (Eq (m (Either a b))) => Eq (MyEitherT a m b) where
 --  (==) :: a -> a -> Bool
     (==) (MyEitherT a) (MyEitherT b) = a == b
@@ -21,3 +40,25 @@ instance Monad m => Functor (MyEitherT a m) where
 --  fmap :: (a -> b) -> f             a -> f             b
     fmap :: (b -> c) -> MyEitherT a m b -> MyEitherT a m c
     fmap f (MyEitherT a) = MyEitherT $ (fmap . fmap) f a
+
+instance Monad m => Applicative (MyEitherT a m) where
+--  pure :: b -> f             b
+    pure :: b -> MyEitherT a m b
+    pure b = MyEitherT $ (pure . pure) b
+
+--  (<*>) :: f (b -> c) -> f b -> f c
+    (<*>) :: MyEitherT a m (b -> c) -> MyEitherT a m b -> MyEitherT a m c
+    (<*>) (MyEitherT fbc) (MyEitherT b) = MyEitherT $ (<*>) <$> fbc <*> b
+
+instance Monad m => Monad (MyEitherT a m) where
+--  return :: b -> t             b
+    return :: b -> MyEitherT a m b
+    return = pure
+
+--  (>>=) :: t             b -> (b -> t             c) -> t             c
+    (>>=) :: MyEitherT a m b -> (b -> MyEitherT a m c) -> MyEitherT a m c
+    (>>=) (MyEitherT b) f = MyEitherT $ do
+                                v <- b
+                                case v of
+                                    Left a  -> return (Left a)
+                                    Right c -> runMyEitherT (f c)
--- Haskell/HaskellProgramming/Chapter26/EitherT/test/EitherTSpec.hs
+++ Haskell/HaskellProgramming/Chapter26/EitherT/test/EitherTSpec.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE UndecidableInstances #-}
 module EitherTSpec (spec) where
 
-import Data.Maybe (isNothing)
 import Test.Hspec
 import Test.Hspec.QuickCheck ( prop
                              , modifyMaxSuccess)
@@ -14,9 +13,9 @@ import Test.QuickCheck (Property)
 import Test.QuickCheck.Monadic ( monadicIO
                                , run)
 import Test.QuickCheck.Checkers
-import Test.QuickCheck.Classes (functor)
-import Data.Either (isLeft)
-import Data.Maybe (fromJust)
+import Test.QuickCheck.Classes ( functor
+                               , applicative
+                               , monad)
 
 import Lib
     ( MyEitherT (..)
@@ -37,74 +36,30 @@ instance (Eq a, Eq b, Eq (Maybe (Either a b))) => EqProp (MyEitherT a Maybe b) w
 
 spec :: Spec
 spec = do
-    describe "Functor laws" $ do
-            prop "Just 1" $
-                functorJustProp 1
-            prop "Just 39" $
-                functorJustProp 39
-            prop "Nothing"
-                functorNothingProp
-            modifyMaxSuccess (const 1) $ do
-                prop "Checkers - Functor"
-                    sTraversableLawsProp
-    -- describe "Applicative laws" $ do
-    --         prop "Just-Just"
-    --             applicativeJustJustProp
-    --         prop "Just-Nothing"
-    --             applicativeJustNothingProp
-    --         prop "Nothing-Just"
-    --             applicativeNothingJustProp
-    --         prop "Nothing-Nothing"
-    --             applicativeNothingNothingProp
+    describe "Functor laws" $
+        modifyMaxSuccess (const 1) $
+            prop "Checkers - Functor" functorCheckers
+    describe "Functor laws" $
+        modifyMaxSuccess (const 1) $
+            prop "Checkers - Applicative" applicativeCheckers
+    describe "Functor laws" $
+        modifyMaxSuccess (const 1) $
+            prop "Checkers - Monad" monadCheckers
 
--- monadicIO :: PropertyM IO a -> Property
--- run :: Monad m => m a -> PropertyM m a
-functorJustProp :: Int -> Bool
-functorJustProp a = do
-    let value = MyEitherT $ (return . Right) 1 :: MyEitherT String Maybe Int
-    let result = (+a) <$> value
-    runMyEitherT result == Just (Right (1 + a))
-
-sTraversableLawsProp :: Property
-sTraversableLawsProp = monadicIO $ do
+functorCheckers :: Property
+functorCheckers = monadicIO $ do
     -- result type is Test.QuickCheck.Monadic.PropertyM IO ()
     let trigger = undefined :: MyEitherT String Maybe (Int, Int, Int)
     run $ quickBatch $ functor trigger
 
-functorNothingProp :: Bool
-functorNothingProp = do
-    let value = MyEitherT $ (return $ Left "Error") :: MyEitherT String Maybe Int
-    let result = (+1) <$> value
-    isLeft (fromJust (runMyEitherT result))
+applicativeCheckers :: Property
+applicativeCheckers = monadicIO $ do
+    -- result type is Test.QuickCheck.Monadic.PropertyM IO ()
+    let trigger = undefined :: MyEitherT String Maybe (Int, Int, Int)
+    run $ quickBatch $ applicative trigger
 
--- applicativeJustJustProp :: Property
--- applicativeJustJustProp = monadicIO $ do
---     let mf = MyEitherT Int $ (return . Just) (+1) :: MyEitherT Int IO (Int -> Int)
---     let ma = MyEitherT Int $ (return . Just) 1 :: MyEitherT Int IO Int
---     let result = mf <*> ma
---     r <- run $ runMyEitherT result
---     assert $ r == Just 2
-
--- applicativeJustNothingProp :: Property
--- applicativeJustNothingProp = monadicIO $ do
---     let mf = MyEitherT Int $ (return . Just) (+1) :: MyEitherT Int IO (Int -> Int)
---     let ma = MyEitherT Int $ return Nothing :: MyEitherT Int IO Int
---     let result = mf <*> ma
---     r <- run $ runMyEitherT result
---     assert $ isNothing r
-
--- applicativeNothingJustProp :: Property
--- applicativeNothingJustProp = monadicIO $ do
---     let mf = MyEitherT Int $ return Nothing :: MyEitherT Int IO (Int -> Int)
---     let ma = MyEitherT Int $ (return . Just) 1 :: MyEitherT Int IO Int
---     let result = mf <*> ma
---     r <- run $ runMyEitherT result
---     assert $ isNothing r
-
--- applicativeNothingNothingProp :: Property
--- applicativeNothingNothingProp = monadicIO $ do
---     let mf = MyEitherT Int $ return Nothing :: MyEitherT Int IO (Int -> Int)
---     let ma = MyEitherT Int $ return Nothing :: MyEitherT Int IO Int
---     let result = mf <*> ma
---     r <- run $ runMyEitherT result
---     assert $ isNothing r
+monadCheckers :: Property
+monadCheckers = monadicIO $ do
+    -- result type is Test.QuickCheck.Monadic.PropertyM IO ()
+    let trigger = undefined :: MyEitherT String Maybe (Int, Int, Int)
+    run $ quickBatch $ monad trigger
